d62fb0d27e87053abf39506336938daa9ab5658c
Juan Cruz Rusconi <juancruzrusconi@MacBook-Pro-de-Juan.local>
2023-06-21 00:31:19 -0300

    second commit

--- desafio1.js
+++ desafio1.js
@@ -20,9 +20,6 @@ class ProductManager {
             stock
         }
 
-        const productCode = this.products.find((product) => product.code === productCode)
-        if(productCode) return "Code in use"
-
         this.products.push(product)
         return product
 
@@ -36,4 +33,12 @@ class ProductManager {
     }
 }
 
-addProduct(remera, blanca, 20000, "ruta de imagen", 0000, 10)
+const productManager = new ProductManager()
+
+productManager.addProduct("Remera", "Blanco", 20000, "Ruta de imagen", 1111, 1000);
+productManager.addProduct("Buzo", "Negro", 40000, "Ruta de imagen", 2222, 700);
+productManager.addProduct("Jogger", "Beige", 35000, "Ruta de imagen", 3333, 500);
+productManager.addProduct("Jean", "Azul", 37000, "Ruta de imagen", 4444, 800);
+productManager.addProduct("Campera", "Verde", 70000, "Ruta de imagen", 500, 300);
+
+console.log(productManager.getProducts())
