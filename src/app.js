b57671526c51b33fa61aedc6612f4a09fdebec66
Juan Cruz Rusconi <juancruzrusconi@MacBook-Pro-de-Juan.local>
2023-07-13 23:15:49 -0300

    preentrega

--- src/app.js
+++ src/app.js
@@ -2,32 +2,49 @@ import express from "express"
 import ProductManager from "./ProductManager.js";
 
 const app = express();
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 const productManager = new ProductManager("./products.json");
 
-app.get("/products", async (req, res) => {
-
-    const { limit, price } = req.query;
+app.get("/api/products", async (req, res) => {
+    try {  
+    const { limit } = req.query;
 
     const products = await productManager.getProducts()
     
     res.send(limit ? 
-        products.slice(0, limit) : products 
-        || 
-        price ? 
-        products.filter((product) => product.price == price) 
-        : products)
+        products.slice(0, limit) 
+        : products) 
+        //|| 
+        //price ? 
+        //products.filter((product) => product.price == price) 
+        //: 
+        //products)
+    } catch {
+        res.status(502).send({error : true})
+    }
 })
 
-app.get("/product/:pid", async (req, res) => {
-
+app.get("/api/product/:pid", async (req, res) => {
+    
+    try {
     const { pid } = req.params
     //const productId = products.find((product) => product.id == pid)
     const products = await productManager.getProducts()
     res.send(products.find((product) => product.id == pid));
     //res.send(productId);
+    } catch {
+        res.status(502).send({error : true})
+    }
 })
+
+app.post("/api/products", async (req, res) => {
+
+    const body = req.body;
+    res.send(body);
+
+});
 
 app.listen(8080, () => {
     console.log("escuchando")
